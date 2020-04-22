b9b36e31a5449cf8d189a47685649e0293777fff
Dell Laptop <Dell Laptop@192.168.0.5>
2020-04-22 12:15:53 +0530

    deepesh commit

--- InventoryManagementSystem/src/main/java/com/capgemini/inventorymanagement/entities/RawMaterialDetails.java
+++ InventoryManagementSystem/src/main/java/com/capgemini/inventorymanagement/entities/RawMaterialDetails.java
@@ -1,5 +1,10 @@
 package com.capgemini.inventorymanagement.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "RawMaterialStock")
 public class RawMaterialDetails {
 
 }
