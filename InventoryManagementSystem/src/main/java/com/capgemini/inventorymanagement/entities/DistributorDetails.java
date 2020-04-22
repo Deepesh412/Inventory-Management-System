9f542914c81dd3f6790a35085516fcb50207381c
Sanda1922 <sandaniharika22@gmail.com>
2020-04-22 03:43:55 -0700

    Niharika commit

--- InventoryManagementSystem/src/main/java/com/capgemini/inventorymanagement/entities/DistributorDetails.java
+++ InventoryManagementSystem/src/main/java/com/capgemini/inventorymanagement/entities/DistributorDetails.java
@@ -1,5 +1,81 @@
 package com.capgemini.inventorymanagement.entities;
 
-public class DistributorDetails {
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
-}
+@Entity
+@Table(name = "distributordetails")
+public class DistributorDetails {
+	@Id
+	private int id;
+	private String distributor_name;
+	private String product_name;
+	private String address;
+	private String phonenumber;
+	private String email_id;
+	
+	@OneToOne(mappedBy = "distributordetails")
+	private ProductOrderDetails productorderdetails;
+	
+
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDistributor_name() {
+		return distributor_name;
+	}
+
+	public void setDistributor_name(String distributor_name) {
+		this.distributor_name = distributor_name;
+	}
+
+	public String getProduct_name() {
+		return product_name;
+	}
+
+	public void setProduct_name(String product_name) {
+		this.product_name = product_name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPhonenumber() {
+		return phonenumber;
+	}
+
+	public void setPhonenumber(String phonenumber) {
+		this.phonenumber = phonenumber;
+	}
+
+	public String getEmail_id() {
+		return email_id;
+	}
+
+	public void setEmail_id(String email_id) {
+		this.email_id = email_id;
+	}
+
+	public ProductOrderDetails getProductorderdetails() {
+		return productorderdetails;
+	}
+
+	public void setProductorderdetails(ProductOrderDetails productorderdetails) {
+		this.productorderdetails = productorderdetails;
+	}
+
+	}
+
--- InventoryManagementSystem/src/main/java/com/capgemini/inventorymanagement/entities/ProductOrderDetails.java
+++ InventoryManagementSystem/src/main/java/com/capgemini/inventorymanagement/entities/ProductOrderDetails.java
@@ -1,5 +1,112 @@
 package com.capgemini.inventorymanagement.entities;
 
-public class ProductOrderDetails {
+import java.sql.Date;
 
-}
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+//import javax.validation.constraints.NotBlank;
+//import javax.validation.constraints.NotNull;
+
+
+
+@Entity
+@Table(name = "productOrderDetails")
+public class ProductOrderDetails {
+	
+	@Id
+	private int order_id;
+	
+	
+	private String Item_name;
+	
+	
+	private int quantity_unit;
+	
+	
+	private double price_per_unit;
+	
+	
+	
+	
+	private double total_price;
+	
+
+	private Date order_date;
+	
+	
+	private Date delivery_date;
+	
+
+	private String delivery_status;
+	
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "supplier_id")
+	private DistributorDetails distributordetails;
+	
+	public double getPrice_per_unit() {
+		return price_per_unit;
+	}
+	public void setPrice_per_unit(double price_per_unit) {
+		this.price_per_unit = price_per_unit;
+	}
+
+	
+	public DistributorDetails getDistributordetails() {
+		return distributordetails;
+	}
+	public void setDistributordetails(DistributorDetails distributordetails) {
+		this.distributordetails = distributordetails;
+	}
+	public int getOrder_id() {
+		return order_id;
+	}
+	public void setOrder_id(int order_id) {
+		this.order_id = order_id;
+	}
+	public String getItem_name() {
+		return Item_name;
+	}
+	public void setItem_name(String item_name) {
+		Item_name = item_name;
+	}
+	public int getQuantity_unit() {
+		return quantity_unit;
+	}
+	public void setQuantity_unit(int quantity_unit) {
+		this.quantity_unit = quantity_unit;
+	}
+	public double getTotal_price() {
+		return total_price;
+	}
+	public void setTotal_price(double total_price) {
+		this.total_price = total_price;
+	}
+	public Date getOrder_date() {
+		return order_date;
+	}
+	public void setOrder_date(Date order_date) {
+		this.order_date = order_date;
+	}
+	public Date getDelivery_date() {
+		return delivery_date;
+	}
+	public void setDelivery_date(Date delivery_date) {
+		this.delivery_date = delivery_date;
+	}
+	public String getDelivery_status() {
+		return delivery_status;
+	}
+	public void setDelivery_status(String delivery_status) {
+		this.delivery_status = delivery_status;
+	}
+	}
+
+
+
+
+
